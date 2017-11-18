af2530c0b9a750f84e333ffeb48e9d529a479e1f
Jayden Nguyen <jaydennguyen6669@gmail.com>
2017-11-18 17:33:03 +1100

    critical update

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ class Config:
     command_prefix = 'm.'
     success = ':white_check_mark:'
     fail = ':no_entry_sign:'
-    mute_role_id = '381035421691281408'
-    default_server_role_id = '381042659101507585'
+    mute_role_id = '353500331361042453'
+    default_server_role_id = '352029401518571530'
     mod_role_ids = ['345876301510344704', '352029385361981440', '345876015471525888', '381326249823305738']
     invite = 'https://discordapp.com/oauth2/authorize?client_id=378721481250570240&scope=bot&permissions=2146958591'

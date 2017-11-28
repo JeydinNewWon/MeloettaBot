8ff69c3f9e79c818c342692232548cc98122f1e9
JeydinNewWon <jayden.nguyen@sydstu.catholic.edu.au>
2017-11-28 23:05:04 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ config = c.Config
 HEROKU_KEY = config.heroku_api_key
 is_prod = os.environ.get('ON_HEROKU', None)
 opus_load_status = opus_loader.load_opus_lib()
-extensions = ['commands.miscellaneous', 'commands.moderation', 'commands.educational', 'commands.music']
+extensions = ['commands.miscellaneous', 'commands.moderation', 'commands.educational', 'commands.music', 'commands.pokedex']
 
 bot = commands.Bot(command_prefix=config.command_prefix, description="Meloetta Bot is a bot designed for moderation, music and functions.", pm_help=None)
 bot.remove_command('help')

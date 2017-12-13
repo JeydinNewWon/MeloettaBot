9c2af8a4b87223b073849dfab580d740dd06b390
Jayden Nguyen <jaydennguyen6669@gmail.com>
2017-12-13 20:48:43 +1100

    Cleverbot Release

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import discord
 import os
 import heroku3
-import sys
 import subprocess
 import aiohttp
 import asyncio
+from utils import clever
 from utils import opus_loader
 from utils import config as c
 from discord.ext import commands
@@ -13,6 +13,8 @@ config = c.Config()
 s_id = str(config.mute_role_id)
 d_id = config.default_server_role_id
 mod_ids = config.mod_role_ids
+CLEVERBOT_USER = config.cleverbot_user
+CLEVERBOT_KEY = config.cleverbot_key
 HEROKU_KEY = config.heroku_api_key
 is_prod = os.environ.get('ON_HEROKU', None)
 opus_load_status = opus_loader.load_opus_lib()
@@ -95,6 +97,23 @@ async def on_ready():
 async def on_message(message):
     if message.content == 'rwby':
         await bot.send_message(message.channel, 'is the best anime!')
+
+    if str(message.content).startswith('<@378721481250570240>'):
+        if not CLEVERBOT_USER or not CLEVERBOT_KEY:
+            print("Values not given for Cleverbot Credentials")
+            return
+
+        cn = message.channel
+        query = message.content.split(' ')[1:]
+        query = ' '.join(query)
+        cleverbot = clever.CleverBot(user=CLEVERBOT_USER, key=CLEVERBOT_KEY)
+        rsp = None
+        try:
+            rsp = cleverbot.query(query)
+        except:
+            pass
+
+        await bot.send_message(cn, rsp)
 
     await bot.process_commands(message)
 
--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,5 @@
 import os
 import configparser
-import shutil
 
 
 class Config:
@@ -28,12 +27,9 @@ class Config:
         self.default_server_role_id = config.get('Credentials', 'default_server_role_id', fallback=None)
         self.mod_role_ids = config.get('Credentials', 'mod_role_ids', fallback=None).split(', ')
         self.invite = config.get('Bot', 'invite', fallback=None)
+        self.cleverbot_user = config.get('Credentials', 'cleverbot_user', fallback=None)
+        self.cleverbot_key = config.get('Credentials', 'cleverbot_key', fallback=None)
 
         if not self.token:
             print('ERROR: THE BOT HAS NOT BEEN SUPPLIED WITH A BOT TOKEN.')
             os._exit(1)
-
-
-
-
-

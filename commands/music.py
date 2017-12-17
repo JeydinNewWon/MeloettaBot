ced031bf20f2bcda016d0f941e8d8af4199292f9
Jayden Nguyen <jaydennguyen6669@gmail.com>
2017-12-17 16:30:06 +1100

    update

--- commands/music.py
+++ commands/music.py
@@ -129,9 +129,6 @@ class Queue:
                 continue
 
             self.current = await self.songs.get()
-            print(self.current)
-            print(self.current.title)
-            print(self.song_list)
             self.song_list.remove(self.current)
             await self.bot.send_message(self.current.channel, self.current.on_song_playing())
             self.current.player.start()
